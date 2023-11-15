94e0dec01079fd20588115b0b7041335761bcdc5
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2023-11-15 15:47:35 +0300

    fix: player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("DEPENDENCIES")]
     [SerializeField] PlayerMovementData movementData;
+    [SerializeField] RunnerCameraController cameraController;
 
     private Rigidbody body;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        cameraController.SetTarget(gameObject);
+        cameraController.SetState(RunnerCameraState.FOLLOW);
     }
 
     private void FixedUpdate()
@@ -25,6 +33,10 @@ public class PlayerController : MonoBehaviour
         string vertical = movementData.VerticalInputName;
 
         Vector3 oldPos = transform.position;
+
+        double horizontalValue = Math.Round(Input.GetAxis(horizontal), 2);
+        double verticalValue = Math.Round(Input.GetAxis(horizontal), 2);
+
         Vector3 input = new Vector3(Input.GetAxis(horizontal), 0, Input.GetAxis(vertical));
         Quaternion lookRotation = input == Vector3.zero ? transform.rotation : Quaternion.LookRotation(input * rotateSpeed);
         body.Move((oldPos + input * moveSpeed), lookRotation);
