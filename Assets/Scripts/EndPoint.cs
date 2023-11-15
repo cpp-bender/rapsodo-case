5db8d05126d62acc9c8af9bc836921a9164ee084
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2023-11-15 19:17:09 +0300

    feat: display message when hitting end point

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class EndPoint : MonoBehaviour
 {
+    private PlayerController playerController;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.TryGetComponent<PlayerController>(out playerController))
+        {
+            Debug.Log("Congratulations");
+        }
     }
 }
